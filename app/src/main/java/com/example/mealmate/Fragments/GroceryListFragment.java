139a5f1f6247bab596ddc2e1f9d51999a6b12d08
tautonatautona <ttautona09@gmail.com>
2024-12-13 09:11:45 +0200

    second commit

---
+++ app/src/main/java/com/example/mealmate/Fragments/GroceryListFragment.java
@@ -0,0 +1,25 @@
+package com.example.mealmate.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mealmate.R;
+
+public class MyAccountFragment extends Fragment {
+
+    public MyAccountFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_account, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/mealmate/Utils/ShakeDetector.java
@@ -0,0 +1,4 @@
+package com.example.mealmate.Utils;
+
+public class ShakeDetector {
+}
